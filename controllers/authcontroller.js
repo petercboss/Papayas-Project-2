8be311d7f16e6d41c19d2c050c71df2b8b60edc9
Peter Boss <m@Peters-MacBook-Pro.local>
2018-03-18 12:11:25 -0500

    displaying weather, news, and sports

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,6 +1,5 @@
 exports.signup = (req, res) => res.render('signup');
 exports.signin = (req, res) => res.render('signin');
-exports.index = (req, res) => res.render('index');
 exports.logout = (req, res) => {
     req.session.destroy(err => {
         res.redirect('signin');
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,34 +11,47 @@ module.exports = (app, passport) => {
     app.get('/logout', authController.logout);
 
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect: '/index',
+        successRedirect: '/',
         failureRedirect: '/signup'
     }));
 
     app.post('/signin', passport.authenticate('local-signin', {
-        successRedirect: '/index',
+        successRedirect: '/',
         failureRedirect: '/signin'
     }));
 
-    app.get('/index', isLoggedIn, function (req, res) {
+    app.get('/', isLoggedIn, function (req, res) {
         let data = [
             calendar(),
             email(),
         request(`http://api.openweathermap.org/data/2.5/weather?q=Chicago&units=imperial&appid=b93ca65a1efb368d1b3d4a3af522cd1a`, (err, response, body) => {
             let weather = JSON.parse(body);
             if (weather.main == undefined) {
-                res.render("index", {
-                    data: null
+                res.render('index', {
+                    weather: null
                 });
             } else {
-                let weatherText = `It's ${weather.main.temp} degrees in ${
-                    weather.name
-                }!`;
-                res.render("index", { data: weatherText });
-            }
-        }),
-        request(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=a49ca69ef1f34e03abdc6463849a01bf`, (err, response, body) => {
-            console.log(JSON.parse(body))
+                let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                request(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=a49ca69ef1f34e03abdc6463849a01bf`, (err, response, body) => {
+                    let news = JSON.parse(body);
+                    if (news.articles == undefined) {
+                        res.render('index', { news: null });
+                    } else {
+                        let newsText = `Todays headlines: ${news.articles[0].title}`;
+                        request(`https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=134889`, (err, response, body) => {
+                            let sports = JSON.parse(body);
+                            console.log(sports);
+                            if (sports.teams == undefined) {
+                                res.render('index', { sports: null });
+                            } else {
+                                let sportsText = `My teams: ${sports.teams[0].strTeam}`;
+                                let sportsLogo = `${sports.teams[0].strTeamLogo}`;
+                                res.render('index', { news: newsText, weather: weatherText, sports: sportsText, logo: sportsLogo });
+                            };
+                        });
+                    };
+                });
+            };
         })];
     });
 
